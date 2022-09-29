f8a250df53bb714f5dd7fefcac49849428a6c1cd
Mahmoud Ahmed <82950581+mahmoudahmedibrahim5@users.noreply.github.com>
2022-09-29 14:23:48 +0200

    Add files via upload

--- HAL/KEYPAD/KEYPAD.c
+++ HAL/KEYPAD/KEYPAD.c
@@ -1,16 +1,22 @@
 /*
- * KEYPAD.c.c
+ * KEYPAD.c
  *
  * Created: 27-Sep-22 11:53:51 PM
- * Author : ADMIN
+ * Author : Mahmoud Ahmed
  */ 
 
 #include "KEYPAD.h"
 
 u8 KEYPAD_read(u8 port)
 {
-	u8 r = -1;
-	u8 c = -1;
+	/* row and column are two variables to identify which key is pressed
+	 * row is initialized by 1 and column is initialized by zero
+	 * If no keys are pressed then (((row-1)*4)+column) will be equal zero
+	 * So the function will return value of NO_KEY_PRESSED
+	 */
+	u8 row = 1,column = 0;
+	u8 key[17] = {NO_KEY_PRESSED,1,2,3,'A',4,5,6,'B',7,8,9,'C','*',0,'#','D'};
+	
 	// To read the row
 	DIO_initPin(port, 0, INPUT_PULLUP);
 	DIO_initPin(port, 1, INPUT_PULLUP);
@@ -27,10 +33,10 @@ u8 KEYPAD_read(u8 port)
 	DIO_setPinValue(port, 6, LOW);
 	DIO_setPinValue(port, 7, LOW);
 	
-	if (DIO_readPin(port, 0) == 0) r=1; 
-	else if (DIO_readPin(port, 1) == 0) r=2;
-	else if (DIO_readPin(port, 2) == 0) r=3;
-	else if (DIO_readPin(port, 3) == 0) r=4;
+	if (DIO_readPin(port, 0) == 0) row=1; 
+	else if (DIO_readPin(port, 1) == 0) row=2;
+	else if (DIO_readPin(port, 2) == 0) row=3;
+	else if (DIO_readPin(port, 3) == 0) row=4;
 	
 	
 	// To read the column
@@ -49,10 +55,10 @@ u8 KEYPAD_read(u8 port)
 	DIO_setPinValue(port, 2, LOW);
 	DIO_setPinValue(port, 3, LOW);
 	
-	if (DIO_readPin(port, 4) == 0) c=1;
-	else if (DIO_readPin(port, 5) == 0) c=2;
-	else if (DIO_readPin(port, 6) == 0) c=3;
-	else if (DIO_readPin(port, 7) == 0) c=4;
+	if (DIO_readPin(port, 4) == 0) column=1;
+	else if (DIO_readPin(port, 5) == 0) column=2;
+	else if (DIO_readPin(port, 6) == 0) column=3;
+	else if (DIO_readPin(port, 7) == 0) column=4;
 	
-	return ((r-1)*4 + c);
+	return key[((row-1)*4)+column];
 }
--- HAL/KEYPAD/KEYPAD.h
+++ HAL/KEYPAD/KEYPAD.h
@@ -3,7 +3,10 @@
 
 #include "DIO.h"
 
+#define NO_KEY_PRESSED 10
 u8 KEYPAD_read(u8 port);
 
 
 #endif /* KEYPAD_H_ */
+
+// Connect the most left pin in the keypad to Px0 (PA0,PB0,PC0,PD0)
