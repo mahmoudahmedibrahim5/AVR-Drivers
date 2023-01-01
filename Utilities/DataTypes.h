237566ca944cfb780d51d7f137779dce3d7ac87a
Mahmoud Ahmed <82950581+mahmoudahmedibrahim5@users.noreply.github.com>
2023-01-01 18:29:19 +0200

    Add files via upload

---
+++ Utilities/DataTypes.h
@@ -0,0 +1,23 @@
+/*
+ * DataTypes.h
+ *
+ * Created: 09-Dec-22 6:24:22 PM
+ *  Author: Mahmoud Ahmed Ibrahim
+ */ 
+
+#ifndef DATATYPES_H_
+#define DATATYPES_H_
+
+typedef unsigned char uint8_t;
+typedef signed char sint8_t;
+
+typedef unsigned short int uint16_t;
+typedef signed short int sint16_t;
+
+typedef unsigned long int uint32_t;
+typedef signed long int sint32_t;
+
+typedef unsigned long long int uint64_t;
+typedef signed long long int sint64_t;
+
+#endif /* DATATYPES_H_ */
